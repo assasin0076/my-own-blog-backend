8d6670ae319c148eb76bd37a2318ba8d55087117
victor <VGeneralov@sila.ru>
2026-04-05 23:13:41 +0200

    feat: добавлена обработка CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://lovely-elf-a9add4.netlify.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, Accept',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
